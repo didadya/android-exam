06dbacff6155f6027415485fb6a9a00952c830aa
didadya <outayeb.lidia@gmail.com>
2020-06-13 22:33:38 +0200

    layouts

---
+++ app/src/main/java/com/examen/ledevin/EcranTitre.java
@@ -0,0 +1,14 @@
+package com.examen.ledevin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EcranTitre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecran_titre);
+    }
+}
---
+++ app/src/main/java/com/examen/ledevin/SplachScreen.java
@@ -0,0 +1,14 @@
+package com.examen.ledevin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplachScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach_screen);
+    }
+}
